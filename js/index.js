20482d68f1a6998e7fa4c7e7cc61fadee39bdda5
xavierbrasher <xavier.brasher@gmail.com>
2021-05-22 14:20:52 +0800

    1.1

--- js/index.js
+++ js/index.js
@@ -1,1 +1,6 @@
 console.log("Running...");
+function test() {
+  document.getElementById("title").style.visibility = "hidden";
+  document.getElementById("button1").style.visibility = "hidden";
+  document.getElementById("button2").style.visibility = "hidden";
+}
